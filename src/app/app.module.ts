8b1619f011c26621424b70820b86f096a4dc7e0e
Luca Eros Provenzano <luca.provenzano@mercadolibre.com>
2021-08-27 21:22:26 -0300

    created directories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutmeComponent } from './components/pages/aboutme/aboutme.component';
+import { NavbarComponent } from './components/structure/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    AboutmeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
