c9edee65eab894a9dc3122a78030c4fcd6a8b42e
Luca Eros Provenzano <luca.provenzano@mercadolibre.com>
2021-08-29 16:02:24 -0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutmeComponent } from './components/pages/aboutme/aboutme.component';
+import { ErrorComponent } from './components/pages/error/error.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { LoginComponent } from './components/pages/login/login.component';
+import { SigninComponent } from './components/pages/signin/signin.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about-me', component: AboutmeComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'signin', component: SigninComponent },
+  { path: '404', component: ErrorComponent },
+  { path: '**', component: ErrorComponent }
 ];
 
 @NgModule({
