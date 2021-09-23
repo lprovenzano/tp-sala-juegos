4027fc943857138a16cea079611c08ef84ca9b2f
Luca Eros Provenzano <luca.provenzano@mercadolibre.com>
2021-09-23 01:00:30 -0300

    game develop,2

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -14,24 +14,6 @@ export class HomeComponent implements OnInit {
       title: 'Ahorcado',
       description: 'Es un juego, en el que el objetivo es adivinar una palabra. El jugador deberá ir diciendo letras que le parece que puede contener la palabra. Si acierta, se escriben todas las letras coincidentes, y sino, es un paso más hacia la tan temida horca.',
       path: '/games/hanged'
-    },
-    {
-      image_url: 'https://cdn.andro4all.com/files/2020/11/juegos-telegram.jpg',
-      title: 'title game',
-      description: 'Some quick example text to build on the card title and make up the bulk of the card content.',
-      path: '/'
-    },
-    {
-      image_url: 'https://cdn.andro4all.com/files/2020/11/juegos-telegram.jpg',
-      title: 'title game',
-      description: 'Some quick example text to build on the card title and make up the bulk of the card content.',
-      path: '/'
-    },
-    {
-      image_url: 'https://cdn.andro4all.com/files/2020/11/juegos-telegram.jpg',
-      title: 'title game',
-      description: 'Some quick example text to build on the card title and make up the bulk of the card content.',
-      path: '/'
     }
   ];
 
