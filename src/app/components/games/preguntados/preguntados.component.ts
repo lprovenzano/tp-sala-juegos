4cbd270ee808dd6e64a8502652999f9bcd7c8d22
Luca Eros Provenzano <luca.provenzano@mercadolibre.com>
2021-10-07 01:19:52 -0300

    finish preguntados

--- src/app/components/games/preguntados/preguntados.component.ts
+++ src/app/components/games/preguntados/preguntados.component.ts
@@ -11,9 +11,21 @@ export class PreguntadosComponent implements OnInit {
 
   questions = [];
   selectedQuestion: Question | undefined;
+  userAnswer: boolean | undefined;
+  isDisabled = false;
+  round = 1;
+  points = 0;
+  wellAnswers = 0;
+  badAnswers = 0;
 
   constructor(private preguntadosService: PreguntadosService) {
+    this.getAllQuestionsAndAnswers();
+  }
 
+  ngOnInit(): void {
+  }
+
+  getAllQuestionsAndAnswers(): void {
     this.preguntadosService.getQuestionsWithAnswers().toPromise()
       .then(
         (questions: Question[]) => {
@@ -27,11 +39,39 @@ export class PreguntadosComponent implements OnInit {
       );
   }
 
-  ngOnInit(): void {
+  getRandomQuestion(): any {
+    let randomQuestion = this.questions[Math.floor(Math.random() * this.questions.length)] as Question;
+    if (randomQuestion.title === this.selectedQuestion?.title) {
+      randomQuestion = this.questions[Math.floor(Math.random() * this.questions.length)];
+    }
+    return randomQuestion;
   }
 
-  getRandomQuestion(): any {
-    return this.questions[Math.floor(Math.random() * this.questions.length)];
+  response(option: boolean): void {
+    this.accumStats(option);
+    this.isDisabled = true;
+    this.userAnswer = option;
+    setTimeout(() => this.next(), 3500);
+  }
+
+  reset(): void {
+    this.points = 0;
+    this.round = 1;
+    this.wellAnswers = 0;
+    this.badAnswers = 0;
+    this.next();
+  }
+
+  private accumStats(option: boolean): void {
+    this.points = option ? this.points + 10 : this.points;
+    option ? this.wellAnswers++ : this.badAnswers++;
+    this.round++;
+  }
+
+  private next(): void {
+    this.userAnswer = undefined;
+    this.isDisabled = false;
+    this.selectedQuestion = this.getRandomQuestion();
   }
 
 }
